b36ab6ecac1cc97dc8ff317e0b147048f434dd18
Jake Shasteen <jakeshasteen@gmail.com>
2016-03-15 11:29:19 -0700

    Add camera component

---
+++ app/components/AR.js
@@ -0,0 +1,36 @@
+import React, { AppRegistry, Component, StyleSheet, Dimensions, View } from 'react-native';
+import Camera from 'react-native-camera';
+
+export default class AR extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+        </Camera>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  preview: {
+    position: 'absolute',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+  },
+});
+
+AppRegistry.registerComponent('AR', () => AR);
