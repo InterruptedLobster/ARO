4b13ca03d2292ae8a5bf520e5ff1b8230c635539
Tiffany <tet226@nyu.edu>
2016-03-22 21:21:47 -0700

    set up structure of signup component

--- app/containers/container_signUp.js
+++ app/containers/container_signUp.js
@@ -1,13 +1,49 @@
 import React, { Component, StyleSheet, View, Image, Text, TextInput  } from 'react-native';
+import Button from 'react-native-button';
 import { connect } from 'react-redux';
-import signUp from '../actions/action_signUp';
+import { signUp }from '../actions/action_user';
 import { bindActionCreators } from 'redux';
 import * as userActions from '../actions/action_user';
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName : '',
+      lastName : '',
+      email : '',
+      number : 0
+    };
+  }
+  onSubmit() {
+
+  }
+
   render() {
+    const {firstName, lastName, email, } = this.state
+    return (
     <View>
+      <View>
+      <TextInput
+        onChangeText={(text) => this.setState({text})}
+        value={firstName}
+      />
+      </View>
+      <View>
+      <TextInput
+        onChangeText={(text) => this.setState({text})}
+        value={lastName}
+      />
+      </View>
+      <View>
+      <TextInput
+        onChangeText={(text) => this.setState({text})}
+        value={email}
+      />
+      </View>
+      <Button text="Signup" clickAction={this.onSubmit.bind(this)}> </Button>
     </View>
+    )
   }
 }
 function mapStateToProps(state) {
@@ -17,7 +53,7 @@ function mapStateToProps(state) {
 }
 function mapDispatchToProps(dispatch) {
   return {
-    action: bindActionCreators()
+    action: bindActionCreators(userActions, dispatch)
   }
 }
-export default connect(mapStateToProps, {signUp})(SignUp);
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
