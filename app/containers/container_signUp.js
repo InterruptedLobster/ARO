207082cdcd1537c867f47e17cfb772132d07088d
Tiffany <tet226@nyu.edu>
2016-03-22 19:29:32 -0700

    start on signup container

--- app/containers/container_signUp.js
+++ app/containers/container_signUp.js
@@ -1,12 +1,16 @@
-import React, { Component, StyleSheet, View, Text  } from 'react-native';
+import React, { Component, StyleSheet, View, Image, Text, TextInput  } from 'react-native';
+import { connect } from 'react-redux';
+import signUp from '../actions/action_signUp.js';
 
 class SignUp extends Component {
-
-
+  render() {
+    <View>
+    </View>
+  }
 }
 function mapStateToProps(state) {
   return {
     user: state.user
-  }
+  };
 }
-export default connect(mapStateToProps)(SignUp);
+export default connect(mapStateToProps, {signUp})(SignUp);
