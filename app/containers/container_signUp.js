e6f03da52a50517065c3f580501db9e924aac61f
Tiffany <tet226@nyu.edu>
2016-03-22 19:48:06 -0700

    refactor to combine all actions related to auth

--- app/containers/container_signUp.js
+++ app/containers/container_signUp.js
@@ -1,6 +1,8 @@
 import React, { Component, StyleSheet, View, Image, Text, TextInput  } from 'react-native';
 import { connect } from 'react-redux';
-import signUp from '../actions/action_signUp.js';
+import signUp from '../actions/action_signUp';
+import { bindActionCreators } from 'redux';
+import * as userActions from '../actions/action_user';
 
 class SignUp extends Component {
   render() {
@@ -12,5 +14,10 @@ function mapStateToProps(state) {
   return {
     user: state.user
   };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    action: bindActionCreators()
+  }
 }
 export default connect(mapStateToProps, {signUp})(SignUp);
