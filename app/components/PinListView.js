a8b1d22851a449d55d8aa963e61d981b25a0bfc0
Alex Bignell <alex@bignell.me>
2016-03-24 01:09:02 -0700

    Create equivalent ARView and PinListView presentational components

---
+++ app/components/PinListView.js
@@ -0,0 +1,61 @@
+import React, { Component, View } from 'react-native';
+import PinList from './PinList';
+import DropNewPinButton from '../containers/container_dropNewPin';
+
+export default class extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'map',
+      currLoc: {
+        latitude: 37.7835551,
+        longitude: -122.4089013,
+      },
+    };
+  }
+
+  componentWillMount() {
+    const { getPins, updateRecent } = this.props;
+    getPins();
+    updateRecent();
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var coords = {};
+        coords.longitude = position.coords.longitude;
+        coords.latitude = position.coords.latitude;
+        this.setState({
+          currLoc: coords
+        });
+      },
+      (error) => {
+        alert(error.message);
+      },
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+
+    this.watchID = navigator.geolocation.watchPosition(
+      (position) => {
+        var coords = {};
+        coords.longitude = position.coords.longitude;
+        coords.latitude = position.coords.latitude;
+      }
+    );
+  }
+
+  render() {
+    const { getLocationToSave, pins, deletePin, recent } = this.props;
+    return (
+      <View style={{flex: 1}}>
+        <PinList
+          deletePin={deletePin}
+          pins={pins}
+          />
+        <DropNewPinButton/>
+      </View>
+    );
+  }
+}
