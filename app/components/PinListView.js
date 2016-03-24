43af0e44a569381a3dd3d8b845c30b81f0d40f1e
Alex Bignell <alex@bignell.me>
2016-03-24 03:10:23 -0700

    Remove geolocation code from PinListView

--- app/components/PinListView.js
+++ app/components/PinListView.js
@@ -6,13 +6,6 @@ export default class extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      view: 'map',
-      currLoc: {
-        latitude: 37.7835551,
-        longitude: -122.4089013,
-      },
-    };
   }
 
   componentWillMount() {
@@ -21,40 +14,15 @@ export default class extends Component {
     updateRecent();
   }
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        var coords = {};
-        coords.longitude = position.coords.longitude;
-        coords.latitude = position.coords.latitude;
-        this.setState({
-          currLoc: coords
-        });
-      },
-      (error) => {
-        alert(error.message);
-      },
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-    );
-
-    this.watchID = navigator.geolocation.watchPosition(
-      (position) => {
-        var coords = {};
-        coords.longitude = position.coords.longitude;
-        coords.latitude = position.coords.latitude;
-      }
-    );
-  }
-
   render() {
-    const { getLocationToSave, pins, deletePin, recent } = this.props;
+    const { pins, deletePin } = this.props;
     return (
       <View style={{flex: 1}}>
         <ViewMenu />
         <PinList
           deletePin={deletePin}
           pins={pins}
-          />
+        />
       </View>
     );
   }
