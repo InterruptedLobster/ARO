0a5c65308c455854b385c0bab7e3a137ad25f660
Alex Bignell <alex@bignell.me>
2016-03-24 02:38:44 -0700

    Import react-native-router-flux to index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@ import rootReducer from './app/reducers/rootReducer.js';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import promise from 'redux-promise';
+import { Router, Scene, Actions as routerActions } from 'react-native-router-flux';
 import ARView from './app/containers/container_ARView';
 import MapView from './app/containers/container_MapView';
 import PinListView from './app/containers/container_PinView';
